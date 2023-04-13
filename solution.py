f5476bafc42d85a5bc47e3ab4b98821b5822a2c4
Ruslan Martynov <62350734+ru-martynov@users.noreply.github.com>
2023-04-13 23:51:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 303100650 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    from scipy.stats import anderson_ksamp
+
+
+    alpha = 0.01
+    Anderson_Darling_test = anderson_ksamp([x, y])
+    stat, p_value = Anderson_Darling_test.statistic, Anderson_Darling_test.pvalue
+    answer = p_value < alpha
+    return answer
